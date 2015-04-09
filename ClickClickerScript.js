61a22c87c339eee76f6c884a7e1832a946965b9e
Elliot Simpson <jsimps72@uncc.edu>
2015-04-09 00:15:16 -0400

    Main Click System
    added click button and click counter

--- ClickClickerScript.js
+++ ClickClickerScript.js
@@ -1,1 +1,20 @@
 //Click Clicker Script File
+
+//Player Variables
+var clicks = 0;
+
+//Document Tags
+var dispClicks = document.getElementById("dispClicks");
+
+//Update the display every 10th of a second
+var updateDisplay = setInterval(updateDisplay, 100);
+
+//The updateDisplay Function
+function updateDisplay(){
+	dispClicks.innerHTML = clicks;
+}
+
+//The click function
+function clickF(){
+	clicks++;
+}
