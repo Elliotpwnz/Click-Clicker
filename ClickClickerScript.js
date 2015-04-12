1dba2978ce655caf06de2d521d6be437a2ff78b8
Elliot Simpson <jsimps72@uncc.edu>
2015-04-12 01:08:46 -0400

    Inventory + Footer
    Added an inventory to the right, as well as a footer. Also fixed default
    underline in the menu.

--- ClickClickerScript.js
+++ ClickClickerScript.js
@@ -2,9 +2,12 @@
 
 //Player Variables
 var clicks = 0;
+var clickBucks = 0;
 
 //Document Tags
 var dispClicks = document.getElementById("dispClicks");
+var dispClicksInv = document.getElementById("dispClicksInv");
+var dispClickBucksInv = document.getElementById("dispClickBucksInv");
 
 //Update the display every 10th of a second
 var updateDisplay = setInterval(updateDisplay, 100);
@@ -14,8 +17,10 @@ function updateDisplay(){
 	//Removes focus from button
 	document.getElementById("clickButton").blur();
 	
-	//Updates Click display
+	//Updates universal display
 	dispClicks.innerHTML = clicks;
+	dispClicksInv.innerHTML = clicks;
+	dispClickBucksInv.innerHTML = clickBucks;
 }
 
 //The click function
