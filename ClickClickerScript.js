857b53a2f4614e5b17af308b1ca1a54acffa30df
Elliot Simpson <jsimps72@uncc.edu>
2015-04-13 23:59:06 -0400

    Added Click Bank tab functionality
    Added content to the click bank. Added a convert button w/
    functionality.

--- ClickClickerScript.js
+++ ClickClickerScript.js
@@ -3,11 +3,13 @@
 //Player Variables
 var clicks = 0;
 var clickBucks = 0;
+var clickBuckValue = 10;
 
 //Document Tags
 var dispClicks = document.getElementById("dispClicks");
 var dispClicksInv = document.getElementById("dispClicksInv");
 var dispClickBucksInv = document.getElementById("dispClickBucksInv");
+var dispClickBuckValue = document.getElementById("dispClickBuckValue");
 
 //Update the display every 10th of a second
 var updateDisplay = setInterval(updateDisplay, 100);
@@ -21,6 +23,18 @@ function updateDisplay(){
 	dispClicks.innerHTML = clicks;
 	dispClicksInv.innerHTML = clicks;
 	dispClickBucksInv.innerHTML = clickBucks;
+	dispClickBuckValue.innerHTML = "(" + clickBuckValue + " Clicks = 1 Click-Buck)";
+	
+}
+
+//The convert button function
+function convertButtonF(){
+	
+	if (clicks >= clickBuckValue){
+		var temp = clicks;
+		clickBucks += Math.floor(clicks / clickBuckValue);
+		clicks = clicks % clickBuckValue;
+	}
 }
 
 //The click function
