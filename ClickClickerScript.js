f48c1e38a988888976626f8c3e31da1b3b621da8
Elliot Simpson <jsimps72@uncc.edu>
2015-04-16 00:13:36 -0400

    Added Inflation/Deflation + News Alerts
    Added a news alert box with news updates occurring every 2 minutes.
    Click-Buck value can either inflate or deflate.

--- ClickClickerScript.js
+++ ClickClickerScript.js
@@ -4,6 +4,7 @@
 var clicks = 0;
 var clickBucks = 0;
 var clickBuckValue = 10;
+var counter = 0;
 
 //Document Tags
 var dispClicks = document.getElementById("dispClicks");
@@ -18,12 +19,42 @@ var updateDisplay = setInterval(updateDisplay, 100);
 function updateDisplay(){
 	//Removes focus from button
 	document.getElementById("clickButton").blur();
+	document.getElementById("convertButton").blur();
+	
+	//Increment the counter
+	counter++;
 	
 	//Updates universal display
 	dispClicks.innerHTML = clicks;
 	dispClicksInv.innerHTML = clicks;
 	dispClickBucksInv.innerHTML = clickBucks;
 	dispClickBuckValue.innerHTML = "(" + clickBuckValue + " Clicks = 1 Click-Buck)";
+	
+	if (counter >= 1200){newsAlert();counter=0;}
+	
+}
+
+//NewsAlert Function
+function newsAlert(){
+	var news = Math.floor((Math.random() * 3));
+	
+	if (clickBuckValue <= 7){news=1;}
+	else if (clickBuckValue >= 13){news=0;}
+	
+	switch(news){
+		
+		case 0:
+			document.getElementById("newsArea").innerHTML = "<b>News Alert:</b> ClickBuck inflation!";
+			clickBuckValue--;
+			break;
+		case 1:
+			document.getElementById("newsArea").innerHTML = "<b>News Alert:</b> ClickBuck deflation!";
+			clickBuckValue++;
+			break;
+		case 2:
+			document.getElementById("newsArea").innerHTML = "<b>News Alert:</b> No news.";
+			break;
+	}
 	
 }
 
